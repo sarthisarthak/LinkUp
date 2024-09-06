edd9167f99257d0209b15c1d5dc8f87930681989
sarthisarthak <140862504+sarthisarthak@users.noreply.github.com>
2024-09-07 00:27:01 +0530

    1_backend_setup

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import mongoose from "mongoose";
+import cors from "cors";
+import { connectToSocket } from "./src/controllers/socketManager.js";
+
+const app = express();
+const server = createServer(app);
+const io = connectToSocket(server);
+
+app.set("port", process.env.PORT || 3000);
+app.use(cors());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }));
+
+const start = async () => {
+  const connectionDb = await mongoose.connect(
+    "mongodb+srv://sarthaksarthi:MS9XGBFbyQtMuoWK@cluster0.aukbsja.mongodb.net/zoom"
+  );
+  console.log("MongoDB Connected");
+  server.listen(app.get("port"), () => {
+    console.log("Listening on PORT 3000");
+  });
+};
+start();
